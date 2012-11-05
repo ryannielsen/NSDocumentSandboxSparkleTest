f043a103f631a7ac831e3e65ba9fa6025d4ec19d
Ryan Nielsen <ryan@tumult.com>
2012-11-05 12:35:56 -0800

    Initial Commit

---
+++ NSDocumentSandboxSparkleTest/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  NSDocumentSandboxSparkleTest
+//
+//  Created by Ryan Nielsen on 11/5/12.
+//  Copyright (c) 2012 Tumult Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ NSDocumentSandboxSparkleTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NSDocumentSandboxSparkleTest
+//
+//  Created by Ryan Nielsen on 11/5/12.
+//  Copyright (c) 2012 Tumult Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
